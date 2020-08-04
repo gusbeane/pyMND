dbb8d3283b023fedd8259568413dc3d6afe38a8a
Gus Beane <angus.beane@cfa.harvard.edu>
2020-08-04 15:17:34 -0400

    Create gen_DMhalo.py

---
+++ gen_DMhalo.py
@@ -0,0 +1,12 @@
+from pyMND import pyMND
+
+CC = 11.0
+V200 = 163.
+LAMBDA = 0.035
+N_GAS = 39606
+N_HALO = 396060 - N_GAS
+MGH = 0.1
+HubbleParam = 1.0
+OutputDir='./'
+OutputFile='MW_ICs'
+t = pyMND(CC, V200, LAMBDA, N_HALO, N_GAS, MGH, HubbleParam, OutputDir, OutputFile)
