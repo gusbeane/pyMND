c4458aa9e90657e11cb3c7ab7431f88087cc65cb
Gus Beane <angus.beane@cfa.harvard.edu>
2020-09-14 17:03:59 -0400

    Remove unecessary potential function

--- pyMND/potential.py
+++ pyMND/potential.py
@@ -1,26 +1,6 @@
 import numpy as np
 from .halo import _halo_potential, _halo_potential_derivative_R
 from .gas_halo import _gas_halo_potential, _gas_halo_potential_derivative_R
-
-def potential(pos, p, u):
-    """
-    The value of the potential from all components.
-    Parameters
-    ----------
-    pos : `~numpy.ndarray` of shape `(N, 3)`
-        Positions at which to compute the value of the potential.
-    p : `~pyMND.param.pyMND_param`
-        pyMND param class.
-    u : `~pyMND.units.pyMND_units`
-        pyMND units class.
-    Returns
-    -------
-    pot : `~numpy.ndarray` of shape `(N)`
-        The value of the potential at pos.
-    """
-    pot = _halo_potential(pos, p.M_HALO, p.RH, u)
-    pot += _gas_halo_potential(pos, p.M_GASHALO, p.RH, u)
-    return pot
     
 def potential_derivative_R(pos, p, u):
     """
